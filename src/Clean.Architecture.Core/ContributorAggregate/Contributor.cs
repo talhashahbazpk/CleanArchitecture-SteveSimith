e2c1a7dac0af9a2fefde1b7fdc32e8987725ba2d
Talha Shahbaz <talhashahbazpk@gmail.com>
2024-08-06 11:37:47 +0500

    Initialize ardalis/strongly-typed-ids

---
+++ src/Clean.Architecture.Core/ContributorAggregate/Contributor.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using Ardalis.GuardClauses;
+using Ardalis.SharedKernel;
+using StronglyTypedIds;
+
+namespace Clean.Architecture.Core.ContributorAggregate;
+
+[StronglyTypedId(backingType:StronglyTypedIdBackingType.Int)]
+public partial struct ContributorId  {}
+
+public class Contributor(string name) : EntityBase<ContributorId>, IAggregateRoot
+{
+  // Example of validating primary constructor inputs
+  // See: https://learn.microsoft.com/en-us/dotnet/csharp/whats-new/tutorials/primary-constructors#initialize-base-class
+  public string Name { get; private set; } = Guard.Against.NullOrEmpty(name, nameof(name));
+  public ContributorStatus Status { get; private set; } = ContributorStatus.NotSet;
+
+  public void UpdateName(string newName)
+  {
+    Name = Guard.Against.NullOrEmpty(newName, nameof(newName));
+  }
+}
